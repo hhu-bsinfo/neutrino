1aba44a40eeb3c4fbe71b81e070e46f894bc5b4d
Filip Krakowski <Filip.Krakowski@Uni-Duesseldorf.de>
2019-05-22 16:27:04 +0200

    Implement ibv_poll_cq

--- example/src/main/java/de/hhu/bsinfo/neutrino/example/App.java
+++ example/src/main/java/de/hhu/bsinfo/neutrino/example/App.java
@@ -22,10 +22,6 @@ public class App {
 
     public static void main(String... args) {
 
-        var attributes = new QueuePair.Attributes();
-
-        LOGGER.info(attributes.toString());
-
         int numDevices = Device.getDeviceCount();
 
         if(numDevices <= 0) {
