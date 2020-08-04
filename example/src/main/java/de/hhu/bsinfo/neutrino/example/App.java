a703bad8aa697bd5e4597ca050d2e7dc19fa63f7
Filip Krakowski <krakowski@hhu.de>
2020-08-04 12:37:19 +0200

    Remove dependency on Spring Boot within example module

--- example/src/main/java/de/hhu/bsinfo/neutrino/example/App.java
+++ example/src/main/java/de/hhu/bsinfo/neutrino/example/App.java
@@ -2,21 +2,12 @@ package de.hhu.bsinfo.neutrino.example;
 
 import de.hhu.bsinfo.neutrino.example.command.Root;
 import de.hhu.bsinfo.neutrino.example.util.InetSocketAddressConverter;
-import java.net.InetSocketAddress;
-
 import de.hhu.bsinfo.neutrino.util.NativeLibrary;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import picocli.CommandLine;
 
-@SpringBootApplication(scanBasePackages = "de.hhu.bsinfo.neutrino")
-@ComponentScan(basePackages = "de.hhu.bsinfo.neutrino",
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.REGEX,
-                pattern = "de\\.hhu\\.bsinfo\\.neutrino\\.example\\.service\\..*"))
-public class App {
+import java.net.InetSocketAddress;
+
+public final class App {
 
     static{
         NativeLibrary.load("neutrino");
@@ -24,6 +15,11 @@ public class App {
 
     @SuppressWarnings("CallToSystemExit")
     public static void main(String... args) {
-        System.exit(SpringApplication.exit(SpringApplication.run(App.class, args)));
+        var exitCode = new CommandLine(new Root())
+                .registerConverter(InetSocketAddress.class, new InetSocketAddressConverter(22222))
+                .setCaseInsensitiveEnumValuesAllowed(true)
+                .execute(args);
+
+        System.exit(exitCode);
     }
 }
--- example/src/main/java/de/hhu/bsinfo/neutrino/example/command/Root.java
+++ example/src/main/java/de/hhu/bsinfo/neutrino/example/command/Root.java
@@ -1,21 +1,15 @@
 package de.hhu.bsinfo.neutrino.example.command;
 
-import de.hhu.bsinfo.neutrino.example.command.arrow.ArrowDemo;
-import de.hhu.bsinfo.neutrino.example.command.arrow.DataGenerator;
-import de.hhu.bsinfo.neutrino.example.command.bench.BenchmarkDemos;
 import de.hhu.bsinfo.neutrino.example.command.panama.PanamaDemo;
-import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 
-@Component
 @CommandLine.Command(
     name = "neutrino",
     description = "",
     subcommands = {
         DeviceInfo.class, MessagingTest.class, RdmaTest.class, WindowTest.class,
         ExtendedDeviceInfo.class, ExtendedMessagingTest.class, ExtendedRdmaTest.class,
-        CommunicationManagerDemo.class, ArrowDemo.class, DataGenerator.class,
-        BenchmarkDemos.class, PanamaDemo.class
+        CommunicationManagerDemo.class, PanamaDemo.class
     }
 )
 public class Root implements Runnable {
